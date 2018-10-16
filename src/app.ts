d0ca0c3d04057b9560013c208e0ee8e6215fbd4b
Zach Hall <zach@sosguy.net>
2018-10-15 22:59:19 -0400

    Selection for MIDI outputs working in Aurelia. Incompatibility message on browsers that don't support it.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import {RouterConfiguration, Router} from 'aurelia-router';
 import { PLATFORM } from 'aurelia-pal';
+import { autoinject } from "aurelia-framework";
+import { MidiService } from 'resources/web-midi/midi-service';
 
+@autoinject
 export class App {
-  constructor(private router: Router) {
+  constructor(private router: Router, private midiService: MidiService) {
   }
   
   configureRouter(config: RouterConfiguration, router: Router): void {
--- src/resources/web-midi/midi-service.ts
+++ src/resources/web-midi/midi-service.ts
@@ -2,11 +2,43 @@ export class MidiService {
   /**
    * Constructs the MIDI service
    */
-  constructor() {
+  constructor(private midiAccess: WebMidi.MIDIAccess, public isActive: boolean, public isUnsupported: boolean) {
+    this.activate();
+  }
+
+  async activate() {
     if (navigator.requestMIDIAccess) {
       console.log('Rarin to rip.');
+
+      this.midiAccess = await navigator.requestMIDIAccess();
+
+      console.log('MIDI ready!');
+
+      // for (let entry of this.midiAccess.inputs) {
+      //   console.log(entry[1]);
+      // }
+      console.log(this.inputNames);
+
+      this.isActive = true;
     } else {
       console.error('This browser does not support web MIDI :(');
+
+      this.isUnsupported = true;
     }
   }
+
+  private getNames(enumerator: Map<string, WebMidi.MIDIPort>) {
+    const names = [];
+    enumerator.forEach(p => names.push(p.name));
+    return names;
+  }
+
+  get inputNames() {
+    return this.getNames(this.midiAccess.inputs);
+  }
+
+  get outputNames() {
+    return this.getNames(this.midiAccess.outputs);
+  }
+  
 }
